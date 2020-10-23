cb72b9d39b012e6ef4160653d77624f0744ee880
Brian Valdez <65619640+BrianVD1220@users.noreply.github.com>
2020-10-22 23:48:36 -0400

    Animated meme

--- meme_v3/sketch.js
+++ meme_v3/sketch.js
@@ -1,16 +1,33 @@
 /*
 	meme version 1
 */
-
+var jet1Image;
+var jet2Image;
 var rotImage;
 var conImage;
 var catImage;
 
+var jet1Width = (50);
+var jet1Height = (50);
+var jet1X = (160);
+var jet1Y = (75);
+var jet1Xspeed = (2);
+var jet1Yspeed = (.5);
+
+var jet2Width = (50);
+var jet2Height = (50);
+var jet2X = (560);
+var jet2Y = (25);
+var jet2Xspeed = (2);
+var jet2Yspeed = (.5); 
+
 function preload() {
 
+	jet1Image = loadImage("jet1.png");
+	jet2Image = loadImage("jet2.png");
 	rotImage = loadImage("dragonrotZoomed.png");
 	conImage = loadImage("contextImage.png");
-	catImage = loadImage("catZilla.JPG");
+	catImage = loadImage("catNo-jet.png");
 
 }
 
@@ -38,20 +55,45 @@ function draw() {
 		textFont("impact");
 		textAlign(CENTER, CENTER);
 		text("The Bosses", width / 5, height / 2 + 275);
+		
+		//Jet 1
+
+		image(jet1Image, jet1X, jet1Y, jet1Width, jet1Height);
 
 		textSize(20);
 		stroke(255);
 		strokeWeight(2);
 		textFont("impact");
 		textAlign(CENTER, CENTER);
-		text("Mist Noble", width / 4, height / 2 - 250);
+		text("Mist Noble", jet1X, jet1Y);
+
+		jet1X += jet1Xspeed;
+		jet1Y += jet1Yspeed;
+
+			if (jet1X > 800 || jet1Y > 810) {
+				jet1X = random(-50, 50);
+				jet1Y = random(-50, 50);
+			}
+
+		//Jet 2
+
+		image(jet2Image, jet2X, jet2Y, jet2Width, jet2Height);
 
 		textSize(20);
 		stroke(255);
 		strokeWeight(2);
 		textFont("impact");
 		textAlign(CENTER, CENTER);
-		text("Dragon Rot", width - 80, height / 2 - 300);
+		text("Dragon Rot", jet2X, jet2Y);
+
+		jet2X -= jet2Xspeed;
+		jet2Y += jet2Yspeed;
+
+			if (jet2X < -100 || jet2Y < -100) {
+				jet2X = random(590, 690);
+				jet2Y = random(-50, 50);
+			}
+
 	} 	
 		else if (key === "d")  {
 
