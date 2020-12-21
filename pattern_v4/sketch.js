74d4eaaa55c50d793efbca60f5110d450c88a38c
Brian Valdez <65619640+BrianVD1220@users.noreply.github.com>
2020-12-21 01:06:53 -0500

    generative pattern

--- pattern_v4/sketch.js
+++ pattern_v4/sketch.js
@@ -1,32 +1,67 @@
 /* 
-	pattern version 3
+	pattern version 4
 */
 
 function setup() {
 	createCanvas(600, 400);
+	pattern();
 }
 
-function draw() {
-	background(180);
+function mousePressed() {
+	pattern();
+}
 
-	let w = 50
-	let h = 50
+function pattern() {
+
+	let r = random(205, 255);
+	let g = random(205, 255);
+	let b = random(205, 255);
+
+	background(r, g, b);
+
+	let w = 50;
+	let h = 50;
 	
 	for (let x = 20; x <= width; x += 25) {
 		for (let y = 20; y <= height; y += 25) {
-		rect(x, y, w - 38, h - 38);
+
+			let r = random(55, 155);
+			let g = random(155, 255);
+			let b = random(55, 155);
+
+			noStroke();
+			fill(r, g, b);
+			rect(x, y, w - 38, h - 38);
+		
 		}
 	}
 
 	for (let x = 0; x <= width; x += 50) {
 		for (let y = 0; y <= height; y += 50) {
-		ellipse(x, y, w, h);
+
+			let r = random(155, 255);
+			let g = random(55, 155);
+			let b = random(55, 155);
+
+			noStroke();
+			fill(r, g, b);
+			ellipse(x, y, w, h);
+
 		}
 	}
 
 	for (let x = 0; x <= width; x += 50) {
 		for (let y = 0; y <= height; y += 50) {
-		ellipse(x, y, w - 25, h - 25);
+
+			let r = random(55, 155);
+			let g = random(55, 155);
+			let b = random(155, 255);
+
+			stroke(r, g, b);
+			strokeWeight(20);
+			noFill();
+			ellipse(x, y, w - 25, h - 25);
+		
 		}
 	}
 }
